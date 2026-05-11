52d85ee9defad3c1fdcbd2ec4263baa33452cbd8
fizhgold29-code <fizhgold29@gmail.com>
2026-05-12 00:33:55 +0100

    Update bolu.go

--- bolu.go
+++ bolu.go
@@ -1,1 +1,20 @@
+package main
 
+import "fmt"
+
+func main(){
+
+fmt.printl(hello basit)
+  }
+
+var(
+first name = msutapha Abdulbasit
+age = 20
+)
+
+var(
+customername = fathia 
+accountbalance = 30
+)
+fmt.printf("customer %s has %ds on their account balance",customername,accountbalance)
+  
