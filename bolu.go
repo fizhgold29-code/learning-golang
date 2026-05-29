2e2cda17a62f6fce0e2dc76b3e239fcf47406789
fizhgold29-code <fizhgold29@gmail.com>
2026-05-29 18:38:24 +0100

    Update bolu.go

--- bolu.go
+++ bolu.go
@@ -1,1 +1,15 @@
-
+Pacakage main
+import "fmt"
+func main() {
+fmt.println ("Hello basit")
+}
+var(
+  players= 4
+  replay= true
+  nameplayerone="khalid"
+)
+func main (){
+  fmt.println(players)
+  fmt.println(replay)
+  fmt.println(nameplayerone)
+  }
